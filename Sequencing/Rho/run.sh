f6d96c34c146d039ed6caebeb5d34e2b5bacd32c
rfriedman22 <ryanfriedman22@gmail.com>
2021-01-29 09:16:19 -0600

    Typo fix in Rho demux

--- Sequencing/Rho/run.sh
+++ Sequencing/Rho/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch --array=1-2 ~/CRX-Information-Content/utils/demultiplexAndBarcodeCount.sh fastqToSample.txt CATGC 9 ~/CRX-Information-Content/utils/
+sbatch --array=1-2 ~/CRX-Information-Content/utils/demultiplexAndBarcodeCount.sh fastqToSequence.txt CATGC 9 ~/CRX-Information-Content/utils/
